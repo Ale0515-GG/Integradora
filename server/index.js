ea9d014dc2473139082fe793b8bdf4346f8e9424
itzel1215 <101162399+itzel1215@users.noreply.github.com>
2024-03-19 22:25:43 -0600

    Usuarios

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import cors from "cors";
 import { routerUsuarios } from "./routes/routerUsuarios.js";
 import { routerSede } from "./routes/routerSede.js"; // Importa routerSede
+import {routerSupAdmi1} from "./routes/routerSupAdmi1.js";
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -17,6 +18,7 @@ mongoose.connect("mongodb://127.0.0.1:27017/gatitos", { useNewUrlParser: true, u
 app.use('/usuarios', routerUsuarios);
 app.use('/sede', routerSede); // Usa routerSede
 app.use('/solicitudes', solicitudRouter);
+app.use('/administradores', routerSupAdmi1);
 
 app.listen(PORT, () => {
     console.log(`Servidor escuchando en el puerto ${PORT}`);
