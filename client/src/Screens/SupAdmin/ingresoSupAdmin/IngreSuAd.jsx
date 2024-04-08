64268a1a0ee87960487e6f220bca7c632513d9f8
Alejandra Galvan <alejandra.glv.gg@gmail.com>
2024-04-07 19:42:51 -0600

    nada

--- client/src/Screens/SupAdmin/ingresoSupAdmin/IngreSuAd.jsx
+++ client/src/Screens/SupAdmin/ingresoSupAdmin/IngreSuAd.jsx
@@ -43,9 +43,13 @@ const IngreSuAd = () => {
     <div class="v130_81"></div>
     <div class="v130_89"></div>
     <div class="v143_42"></div>
-    <div class="v281_26"></div><span class="v281_27">Iniciar Sesión</span><span class="v281_28">Mostrar
-        Contraseña</span>
-        <span class="v281_29"><label>
+    <div class="v281_26"></div>
+
+        <span class="v281_27">Iniciar Sesión</span>
+        <span class="v281_28">Mostrar Contraseña</span>
+
+        <span class="v281_29">
+          <label>
             Ingresar Contraseña:
             <input
               type="password"
@@ -53,7 +57,8 @@ const IngreSuAd = () => {
               onChange={(e) => setAcceso(e.target.value)}
             />
           </label>
-        </span>
+          </span>
+          
         <span class="v281_30">
           <label>
             Ingresar Usuario:
@@ -64,12 +69,15 @@ const IngreSuAd = () => {
             />
           </label>
         </span>
+        
+    <div class="v281_31"></div><span class="v281_32">Cancelar</span>
     <div class="v281_33"></div>
     <div class="v281_34"></div>
     <div class="v281_35"></div>
     <div class="v281_36"></div>
     <div class="v281_37"></div>
-
+    
+    <div class="v281_38"></div><span class="v281_39">Ingresar</span>
     </form>
 </div>
   );
