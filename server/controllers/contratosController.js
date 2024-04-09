c3d75316560e01c9692cba2a4e79cccf41932d12
PalomaresAndrea <palomaresschoenstantt@gmail.com>
2024-04-09 14:05:15 -0600

    casi

---
+++ server/controllers/contratosController.js
@@ -0,0 +1,63 @@
+import Contrato from '../models/contrato.js';
+
+export const getContratos = async (req, res) => {
+  try {
+    const data = await Contrato.find({});
+    res.json({ success: true, data: data });
+  } catch (error) {
+    console.error("Error al obtener los contratos:", error);
+    res.status(500).json({ success: false, message: "Error del servidor" });
+  }
+};
+
+export const getContratoById = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const data = await Contrato.findById(id);
+    res.json({ success: true, data: data });
+  } catch (error) {
+    console.error("Error al obtener el contrato:", error);
+    res.status(500).json({ success: false, message: "Error del servidor" });
+  }
+};
+
+exports.agregarContrato = async (req, res) => {
+  try {
+    const nuevoContrato = new Contrato(req.body);
+    const contratoGuardado = await nuevoContrato.save();
+    res.status(201).json({ success: true, data: contratoGuardado });
+  } catch (error) {
+    console.error("Error al agregar un contrato:", error);
+    res.status(500).json({ success: false, message: "Error del servidor" });
+  }
+};
+
+exports.actualizarContrato = async (req, res) => {
+  const { id } = req.params;
+  const newData = req.body;
+
+  try {
+    const contratoActualizado = await Contrato.findByIdAndUpdate(id, newData, { new: true });
+    res.json({ success: true, message: "Contrato actualizado exitosamente", data: contratoActualizado });
+  } catch (error) {
+    console.error("Error al actualizar el contrato:", error);
+    res.status(500¿).json({ success: false, message: "Error del servidor" });
+
+  }
+};
+
+exports.eliminarContrato = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const contratoEliminado = await Contrato.findByIdAndDelete(id);
+    res.json({ success: true, message: "Contrato eliminado exitosamente", data: contratoEliminado });
+  } catch (error) {
+    console.error("Error al eliminar el contrato:", error);
+    res.status(500¿).json({ success: false, message: "Error del servidor" });
+
+  }
+};
+
+export default (getContratos, getContratoById,agregarContrato,eliminarContrato,actualizarContrato);
