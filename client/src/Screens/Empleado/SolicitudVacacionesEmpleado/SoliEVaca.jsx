2d7a04d0a31664b1044192fa6f7c22f56b5d9080
itzel1215 <101162399+itzel1215@users.noreply.github.com>
2024-04-11 20:31:31 -0600

    mas

--- client/src/Screens/Empleado/SolicitudVacacionesEmpleado/SoliEVaca.jsx
+++ client/src/Screens/Empleado/SolicitudVacacionesEmpleado/SoliEVaca.jsx
@@ -8,9 +8,13 @@ import { Link } from "react-router-dom";
 const SolicitudesVacacionesVista = () => {
   const [fechasSeleccionadas, setFechasSeleccionadas] = useState([]);
   const [error, setError] = useState('');
-  const [mostrarFormulario, setMostrarFormulario] = useState(false); // Estado para mostrar el formulario de solicitud
-  const [historialSolicitudes, setHistorialSolicitudes] = useState([]); // Estado para almacenar el historial de solicitudes
-  const [empleado, setEmpleado] = useState(null); // Estado para almacenar los datos del empleado
+  const [mostrarFormulario, setMostrarFormulario] = useState(false); 
+  const [historialSolicitudes, setHistorialSolicitudes] = useState([]);
+  const [empleado, setEmpleado] = useState(null);
+  const [mostrarModificar, setMostrarModificar] = useState(false); 
+  const [solicitudSeleccionada, setSolicitudSeleccionada] = useState(null);
+  const [fechaIni, setFechaIni] = useState(null);
+  const [fechaTer, setFechaTer] = useState(null);
 
   const handleDateChange = (date) => {
     setFechasSeleccionadas(date);
@@ -24,13 +28,12 @@ const SolicitudesVacacionesVista = () => {
 
       await Axios.post("http://localhost:3001/solicitudVacaciones/create", {
         fechas: fechasTexto,
-        // Agrega los demás datos de la solicitud aquí si es necesario
       });
 
       setError("");
-      obtenerHistorialSolicitudes(); // Actualizar el historial después de guardar una solicitud
-      setMostrarFormulario(false); // Ocultar el formulario después de guardar o modificar la solicitud
-      setFechasSeleccionadas([]); // Limpiar las fechas seleccionadas
+      obtenerHistorialSolicitudes(); 
+      setMostrarFormulario(false); 
+      setFechasSeleccionadas([]); 
     } catch (error) {
       console.error("Error al solicitar las vacaciones:", error.message);
       setError("Error al solicitar las vacaciones");
@@ -40,36 +43,68 @@ const SolicitudesVacacionesVista = () => {
   const obtenerHistorialSolicitudes = async () => {
     try {
       const response = await Axios.get("http://localhost:3001/solicitudVacaciones");
-      setHistorialSolicitudes(response.data.data); // Suponiendo que el servidor devuelve un objeto con una propiedad 'data' que contiene el historial de solicitudes
+      setHistorialSolicitudes(response.data.data); 
     } catch (error) {
       console.error('Error al obtener el historial de solicitudes:', error.message);
-      // Aquí puedes manejar el error de acuerdo a tus necesidades
     }
   };
 
   useEffect(() => {
     obtenerHistorialSolicitudes();
-    // Simulando la carga de los datos del empleado
     const empleadoSimulado = {
       nombreempleado: 'Alejandra',
       sede: 'UTNG',
       area: 'RH'
-      // Agrega más datos del empleado según sea necesario
     };
     setEmpleado(empleadoSimulado);
   }, []);
 
+
   const solicitarVacaciones = () => {
     setMostrarFormulario(true);
-    setFechasSeleccionadas([]); // Limpiar las fechas seleccionadas al solicitar nuevas vacaciones
+    setFechasSeleccionadas([]);
+    setMostrarModificar(false); 
   };
 
-  // Función para eliminar una solicitud
+  const modificarSolicitud = async () => {
+    try {
+      console.log("Fecha de inicio seleccionada:", fechaIni);
+      console.log("Fecha de fin seleccionada:", fechaTer);
+      console.log("Solicitud seleccionada:", solicitudSeleccionada);
+
+      if (solicitudSeleccionada) {
+        const id = solicitudSeleccionada._id;
+        const fechasTexto = [fechaIni.toISOString().split("T")[0], fechaTer.toISOString().split("T")[0]];
+  
+        await Axios.put(
+          `http://localhost:3001/solicitudVacaciones/update/${id}`,
+          { fechas: fechasTexto }
+        );
+  
+        setError("");
+        await obtenerHistorialSolicitudes(); // Esperar a que se complete la actualización antes de obtener el historial
+        setMostrarFormulario(false); 
+        setFechasSeleccionadas([]); 
+        setMostrarModificar(false); 
+      }
+    } catch (error) {
+      console.error("Error al modificar la solicitud:", error.message);
+      setError("Error al modificar la solicitud");
+    }
+  };
+  
+  const seleccionarSolicitud = (solicitud) => {
+    setSolicitudSeleccionada(solicitud); 
+    setFechaIni(new Date(solicitud.fechaIni)); 
+    setFechaTer(new Date(solicitud.fechaTer)); 
+    setMostrarModificar(true); 
+  };
+  
   const eliminarSolicitud = async (solicitudId) => {
     try {
       await Axios.delete(`http://localhost:3001/solicitudVacaciones/delete/${solicitudId}`);
       setError('');
-      obtenerHistorialSolicitudes(); // Actualizar el historial después de eliminar una solicitud
+      obtenerHistorialSolicitudes();
     } catch (error) {
       console.error('Error al eliminar la solicitud:', error.message);
       setError('Error al eliminar la solicitud');
@@ -79,7 +114,8 @@ const SolicitudesVacacionesVista = () => {
   return (
     <div className="solicitudes-container">
       <div className="solicitudes-header">
-        <div className="logo"></div>
+      <div className="logo-container">
+        <div className="logo"></div></div>
         <Link to="/NavegacionEmpleado" className="regresar"></Link>
         <h1 className="solicitudes-title">Solicitud de Vacaciones</h1>
       </div>
@@ -90,7 +126,6 @@ const SolicitudesVacacionesVista = () => {
           <p><strong>Nombre:</strong> {empleado.nombreempleado}</p>
           <p><strong>Sede:</strong> {empleado.sede}</p>
           <p><strong>Área:</strong> {empleado.area}</p>
-          {/* Agrega más campos del empleado según sea necesario */}
         </div>
       )}
 
@@ -103,6 +138,19 @@ const SolicitudesVacacionesVista = () => {
           <Calendar onChange={handleDateChange} value={fechasSeleccionadas} selectRange />
           <div className="boton-container">
             <button className="solicitud-boton" onClick={guardarSolicitud}>Guardar</button>
+          </div>
+        </div>
+      )}
+
+      {mostrarModificar && (
+        <div className="modificar-formulario">
+          <h2>Modificar Solicitud</h2>
+          <label>Fecha de Inicio:</label>
+          <Calendar onChange={setFechaIni} value={fechaIni} />
+          <label>Fecha de Fin:</label>
+          <Calendar onChange={setFechaTer} value={fechaTer} />
+          <div className="boton-container">
+            <button className="solicitud-boton" onClick={modificarSolicitud}>Modificar</button>
           </div>
         </div>
       )}
@@ -130,6 +178,7 @@ const SolicitudesVacacionesVista = () => {
                   <td>{solicitud.fechaIni}</td>
                   <td>{solicitud.fechaTer}</td>
                   <td>
+                    <button onClick={() => seleccionarSolicitud(solicitud)}>Modificar</button>
                     <button onClick={() => eliminarSolicitud(solicitud._id)}>Eliminar</button>
                   </td>
                 </tr>
@@ -145,3 +194,4 @@ const SolicitudesVacacionesVista = () => {
 };
 
 export default SolicitudesVacacionesVista;
+
